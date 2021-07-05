5431128fba1082fdcb4aacbb1f37255ecf8cf4a4
Taylor Smith <tbsmith0205@gmail.com>
2021-07-05 15:00:02 -0600

    employee test and all lib files functioning properly

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,19 +1,45 @@
 const { it } = require("@jest/globals");
 const { describe } = require("yargs");
 const Employee = require("../lib/Employee");
-const Employee = require("../lib/Employee");
 
-describe("Employee", () => {
-  // can create new object
-  describe("Initialization", () => {
-    it("should create an object with name, id, and email properites", () => {
-      // Arrange
-      const employee = new Employee();
+// describe("Employee", () => {
+// can create new object
 
-      expect(typeof employee).toEqual(typeof "object");
-    });
-  });
-  // can create new instance of employee
-  // set name via constructor arguments, id , email
-  // test get functions
+test("should create an object with name, id, and email properites", () => {
+  // Arrange
+  const employee = new Employee();
+
+  expect(typeof employee).toBe("object");
 });
+test("can set name via constructor arguments", () => {
+  const employee = new Employee("Bob");
+  expect(employee.name).toBe("Bob");
+});
+test("can set id via constructor arguments", () => {
+  const employee = new Employee("Bob", 1);
+  expect(employee.id).toBe(1);
+});
+test("can set email via constructor arguments", () => {
+  const employee = new Employee("Bob", 1, "bob@gmail.com");
+  expect(employee.email).toBe("bob@gmail.com");
+});
+
+test("can get name via getName", () => {
+  const employee = new Employee("Bob");
+  expect(employee.getName()).toBe("Bob");
+});
+test("can get name via getId", () => {
+  const employee = new Employee("Bob", 1);
+  expect(employee.getId()).toBe(1);
+});
+test("can get name via getEmail", () => {
+  const employee = new Employee("Bob", 1, "bob@gmail.com");
+  expect(employee.getEmail()).toBe("bob@gmail.com");
+});
+
+test("can get name via getTitle", () => {
+  const employee = new Employee("Bob", 1, "bob@gmail.com");
+  expect(employee.getTitle()).toBe("Employee");
+});
+
+// });
--- index.js
+++ index.js
@@ -1,76 +1,175 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const jest = require("jest");
-const path = require();
+// const path = require();
 
-const employee = require("./lib/Employee");
-const engineer = require("./lib/Engineer");
-const intern = require("./lib/Intern");
-const manager = require("./lib/Manager");
+const Employee = require("./lib/Employee.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+// const manager = require("./lib/Manager");
+const Manager = require("./lib/Manager.js");
+
+const generateHTML = require("./src/page-template.js");
+
+const teamArray = [];
+landingPagePrompt();
+function landingPagePrompt() {
+  inquirer
+    .prompt([
+      {
+        message:
+          "Welcome to the Team Generator. Please supply the name of your team:",
+        name: "teamName",
+        type: "input",
+      },
+    ])
+    .then(function (data) {
+      const teamName = data.teamName;
+      teamArray.push(teamName);
+      addManager();
+    });
+}
 
 function addManager() {
-  inquirer.prompt([
-    {
-      message: "What is your manager's name?",
-      name: "name",
-    },
-    {
-      message: "What is your manager's email?",
-      name: "email",
-    },
-    {
-      message: "What is your manager's office number?",
-      name: "officeNumber",
-      type: "number",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your manager's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter your manager's ID number.",
+      },
+      {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "email",
+      },
+      {
+        message: "What is your manager's office number?",
+        name: "officeNumber",
+        type: "input",
+      },
+    ])
+    .then(function (data) {
+      const name = data.name;
+      const id = data.id;
+      const email = data.email;
+      const officeNumber = data.officeNumber;
+      const teamMember = new Manager(name, id, email, officeNumber);
+      teamArray.push(teamMember);
+      addOthers();
+    });
 }
 
 function addOthers() {
-  inquirer.prompt([
-    {
-      message: "Would you like to add more team members?",
-      choices: [
-        "Yes, add an engineer",
-        "Yes, add an intern",
-        "No, all team members have been added",
-      ],
-      name: "addTeamMembers",
-      type: "list",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        message: "Would you like to add more team members?",
+        choices: [
+          "Yes, I would like to add an engineer.",
+          "Yes, I would like to add an intern.",
+          "No, all team members have been added.",
+        ],
+        name: "addOthers",
+        type: "list",
+      },
+    ])
+    .then(function (data) {
+      switch (data.addOthers) {
+        case "Yes, I would like to add an engineer.":
+          addEngineer();
+          break;
+
+        case "Yes, I would like to add an intern.":
+          addIntern();
+          break;
+
+        case "No, all team members have been added.":
+          console.log(teamArray);
+      }
+    });
 }
 
 function addEngineer() {
-  inquirer.prompt([
-    {
-      message: "What is the engineer's name?",
-      name: "name",
-    },
-    {
-      message: "What is the engineer's email?",
-      name: "email",
-    },
-    {
-      message: "What is the engineer's GitHub username?",
-      name: "github",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's GitHub username?",
+        name: "github",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter your engineer's ID number.",
+      },
+    ])
+    .then(function (data) {
+      const name = data.name;
+      const github = data.github;
+      const email = data.email;
+      const id = data.id;
+
+      const teamMember = new Engineer(name, id, email, github);
+      teamArray.push(teamMember);
+      addOthers();
+    });
 }
 
 function addIntern() {
-  inquirer.prompt([
-    {
-      message: "What is the intern's name?",
-      name: "name",
-    },
-    {
-      message: "What is the intern's email?",
-      name: "email",
-    },
-    {
-      message: "What school does this intern attend?",
-      name: "school",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the intern's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What school does this intern attend?",
+        name: "school",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter your intern's ID number.",
+      },
+    ])
+    .then(function (data) {
+      const name = data.name;
+      const school = data.school;
+      const email = data.email;
+      const id = data.id;
+
+      const teamMember = new Intern(name, id, email, school);
+      teamArray.push(teamMember);
+      addOthers();
+    });
+}
+
+function completeTeam() {
+  console.log("All team members have been added, and your team is complete.");
+
+  const htmlTeamArray = [];
+  const htmlGeneration = ``;
 }
