c9b07ff8086b8a6b8623ee5ad3707d91a486f924
Taylor Smith <tbsmith0205@gmail.com>
2021-07-05 15:12:05 -0600

    engineer test working

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,25 @@
+const Engineer = require("../lib/Engineer.js");
+
+describe("Engineer", () => {
+  describe("Initialization", () => {
+    it("should create an object with a name, email, github, and id properties", () => {
+      const engineer = new Engineer("John", 2, "john@gmail.com", "john123");
+
+      expect(engineer.name).toEqual("John");
+      expect(engineer.id).toEqual(2);
+      expect(engineer.email).toEqual("john@gmail.com");
+      expect(engineer.github).toEqual("john123");
+    });
+  });
+  describe("Functions", () => {
+    it("should provide info when the appropriate function is called", () => {
+      const engineer = new Engineer("John", 2, "john@gmail.com", "john123");
+
+      expect(engineer.getName()).toEqual("John");
+      expect(engineer.getId()).toEqual(2);
+      expect(engineer.getEmail()).toEqual("john@gmail.com");
+      expect(engineer.getTitle()).toEqual("Engineer");
+      expect(engineer.getGitHub()).toEqual("john123");
+    });
+  });
+});
